8d6e8592880260a1c566ac46cbb02de65314f7c4
sagarashi787 <sagarashi871@gmail.com>
2025-08-19 20:44:53 +0400

    Add change st

--- src/main/java/com/bank/models/User.java
+++ src/main/java/com/bank/models/User.java
@@ -1,4 +1,22 @@
 package com.bank.models;
 
 public class User {
+
+    private int id;
+    private String name;
+    private String accounts;
+
+    public User(int id, String name, String accounts) {
+        this.id = id;
+        this.name = name;
+        this.accounts = accounts;
+    }
+
+    public String getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount() {}
+//    public void removeAccount() {}
+
 }
