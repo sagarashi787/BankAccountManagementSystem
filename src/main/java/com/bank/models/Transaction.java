e0492db66e99813e8795c5eb059ac4f24cd18025
sagarashi787 <sagarashi871@gmail.com>
2025-08-20 23:07:09 +0400

    Add change tr

--- src/main/java/com/bank/models/Transaction.java
+++ src/main/java/com/bank/models/Transaction.java
@@ -1,4 +1,20 @@
 package com.bank.models;
 
 public class Transaction {
+
+    private int id;
+    private int amount;
+    private String type;
+    private int date;
+    private String sourceAccount;
+    private String targetAccount;
+
+    public Transaction(int id, int amount, String type, int date, String sourceAccount, String targetAccount) {
+        this.id = id;
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+        this.sourceAccount = sourceAccount;
+        this.targetAccount = targetAccount;
+    }
 }
--- src/main/java/com/bank/services/BankService.java
+++ src/main/java/com/bank/services/BankService.java
@@ -1,4 +1,16 @@
 package com.bank.services;
 
 public class BankService {
+
+    private String createAccount;
+    private int transfer;
+    private int getTransactionHistory;
+    private int getTotalBalance;
+
+    public BankService(String createAccount, int transfer, int getTransactionHistory, int getTotalBalance) {
+        this.createAccount = createAccount;
+        this.transfer = transfer;
+        this.getTransactionHistory = getTransactionHistory;
+        this.getTotalBalance = getTotalBalance;
+    }
 }
